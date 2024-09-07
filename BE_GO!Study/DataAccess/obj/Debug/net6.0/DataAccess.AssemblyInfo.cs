4e06fcc15ad5c69f201bf0783854d6d8c0993338
khoa phung <110983505+dangkhoadoiqwe@users.noreply.github.com>
2024-09-07 18:15:42 +0700

    .
    .

--- BE_GO!Study/DataAccess/obj/Debug/net6.0/DataAccess.AssemblyInfo.cs
+++ BE_GO!Study/DataAccess/obj/Debug/net6.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea8379cbfb571e9689a919cd26640e6eaa917da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24df5e646f189ad107be32cb12984e5b922ba0af")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
